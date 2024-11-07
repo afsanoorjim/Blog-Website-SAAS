e04582eaeebbf58bcaa12eeec0616b1cb48011d7
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-11-07 11:58:01 +0000

    add

--- bloghub/bloghub/settings.py
+++ bloghub/bloghub/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'Blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
