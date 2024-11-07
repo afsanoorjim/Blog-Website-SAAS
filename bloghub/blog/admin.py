e97f7dffc9d1030e92608c09e07c681fe8af5d27
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-11-07 15:39:46 +0000

    add

--- bloghub/blog/admin.py
+++ bloghub/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+
+
+# class BlogAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'publish_date')
+#     list_filter = ('author', 'publish_date')
+#     search_fields = ('title', 'content')
+#     date_hierarchy = 'publishdate'
+
+# admin.site.register(Blog)
--- bloghub/blog/models.py
+++ bloghub/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog:
+    author = models.CharField(max_length=4000)
+    title = models.CharField(max_length=4000)
+    content = models.TextField()
+    content_image = models.ImageField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
