01458cf288f2f1c3896c10b8348f99961ebadd41
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-11-07 15:43:06 +0000

    add

--- bloghub/blog/admin.py
+++ bloghub/blog/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-
-
-
-# class BlogAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'author', 'publish_date')
-#     list_filter = ('author', 'publish_date')
-#     search_fields = ('title', 'content')
-#     date_hierarchy = 'publishdate'
-
-# admin.site.register(Blog)
