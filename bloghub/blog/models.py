fbd21ed4404f3f091e2812313102e2928c27e1d1
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-11-08 08:36:55 +0000

    add

--- bloghub/blog/models.py
+++ bloghub/blog/models.py
@@ -5,8 +5,7 @@ class Blog(models.Model):
     author = models.CharField(max_length=4000)
     title = models.CharField(max_length=4000)
     content = models.TextField()
-    content_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    content_image = models.ImageField()
     publish_date = models.DateTimeField(auto_now_add=True)
-
     def __str__(self) -> str:
         return self.title
--- bloghub/bloghub/settings.py
+++ bloghub/bloghub/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 STATIC_URL = "static/"
-MEDIA_URL = '/media/'  # This is the URL that will be used to access media files in your project
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
