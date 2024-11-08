214557a52bc18f3e454640964588397cff17b1fe
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-11-08 09:19:45 +0000

    add

--- bloghub/blog/models.py
+++ bloghub/blog/models.py
@@ -5,7 +5,7 @@ class Blog(models.Model):
     author = models.CharField(max_length=4000)
     title = models.CharField(max_length=4000)
     content = models.TextField()
-    content_image = models.ImageField()
+    content_image = models.ImageField(upload_to= 'blog/media/', blank=True, null=True)
     publish_date = models.DateTimeField(auto_now_add=True)
     def __str__(self) -> str:
         return self.title
